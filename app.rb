bf41eba9daeb30eeeb2ff42dbaec8919c3f9d085
Murphy Stroppa <Murph@Murphys-MacBook-Pro.local>
2015-05-29 17:59:44 -0700

    Arrays filled with arrays of randomly assorted names

--- app.rb
+++ app.rb
@@ -7,7 +7,16 @@ get "/" do
 end
 
 post "/Randomized" do
+  params.to_s
+  @names = params[:names].split(",").shuffle
+  @num_teams = params[:number].to_i
 
+  @arrays = Array.new(@num_teams) { [] }
+  @names.each_with_index do |name, i|
+    @arrays[i % @num_teams] << name
+  end
+  
   erb :home, layout: :template
+
 
 end
